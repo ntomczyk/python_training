5d3518c20c94c5107513d1654063ca77b436b865
Nina Tomczyk <tomczyk.nina@gmail.com>
2020-04-02 10:44:41 +0200

    Add .gitignore

--- test_adding_group.py
+++ test_adding_group.py
@@ -1,57 +1,67 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+from group import Group
 
 class AddingGroup(unittest.TestCase):
     def setUp(self):
-        self.wb = webdriver.Firefox()
-        self.wb.implicitly_wait(30)
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
 
-    
     def test_adding_group(self):
-        wb = self.wb
-        wb.get("http://localhost/addressbook/index.php")
-        wb.find_element_by_name("pass").send_keys("secret")
-        wb.find_element_by_name("user").send_keys("admin")
-        wb.find_element_by_name("pass").click()
-        wb.find_element_by_xpath("//input[@value='Login']").click()
-        wb.find_element_by_link_text("groups").click()
-        wb.find_element_by_name("new").click()
-        wb.find_element_by_name("group_name").send_keys("test2")
-        wb.find_element_by_name("group_header").send_keys("header")
-        wb.find_element_by_name("group_footer").send_keys("footer")
-        wb.find_element_by_name("submit").click()
-        wb.find_element_by_link_text("group page").click()
-        wb.find_element_by_link_text("Logout").click()
-    
-    def is_element_present(self, how, what):
-        try: self.wb.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wb.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.wb.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username = "secret", password = "admin")
+        self.open_groups_page(wd)
+        self.create_group(wd, Group(group_name ="group1", header ="header1", footer ="footer2"))
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def test_adding_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="secret", password="admin")
+        self.open_groups_page(wd)
+        self.create_group(wd, Group(group_name="", header="", footer=""))
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/index.php")
+
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("pass").send_keys(username)
+        wd.find_element_by_name("user").send_keys(password)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_groups_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+
+    def create_group(self, wd, group):
+        # Add new group
+        wd.find_element_by_name("new").click()
+        # Fill in group form
+        wd.find_element_by_name("group_name").send_keys(group.group_name)
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # Submit groups creation
+        wd.find_element_by_name("submit").click()
+
+
+    def return_to_groups_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+
     def tearDown(self):
-        self.wb.quit()
+        self.wd.quit()
 
 
 if __name__ == "__main__":
