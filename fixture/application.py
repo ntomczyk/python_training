35118f2e080860c8f1cb9bfc40ca249d5864eb9b
Nina Tomczyk <tomczyk.nina@gmail.com>
2020-04-16 19:30:42 +0200

    improving intelligence of login and logout methods

--- fixture/application.py
+++ fixture/application.py
@@ -6,7 +6,7 @@ from fixture.contact import ContactHelper
 class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
@@ -17,7 +17,6 @@ class Application:
             return True
         except:
             return False
-
 
     def open_home_page(self):
         wd = self.wd
--- fixture/session.py
+++ fixture/session.py
@@ -18,6 +18,26 @@ class SessionHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
 
+    def ensure_login(self, username, password):
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username=username, password=password)
+
+    def ensure_logout(self):
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("(//div[@id='top']/form/b)[1]").text == "(" + username + ")"
+
 
 
 
