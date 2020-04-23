d2d221f2ca999baf2482e1328807c0025e5b0272
Nina Tomczyk <tomczyk.nina@gmail.com>
2020-04-23 09:02:31 +0200

    Optimization of page transitions

--- fixture/application.py
+++ fixture/application.py
@@ -6,7 +6,7 @@ from fixture.contact import ContactHelper
 class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(5)
+        self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
@@ -21,6 +21,9 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook")
+        if not (wd.current_url.endswith("/addressbook/") or
+                wd.current_url.endswith("/index.php")):
+             wd.get("http://localhost/addressbook")
 
     def destroy(self):
         self.wd.quit()
