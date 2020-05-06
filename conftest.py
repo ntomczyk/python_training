5619240c1be143be85def35b6e290ff0b7847952
Nina Tomczyk <tomczyk.nina@gmail.com>
2020-05-06 20:27:26 +0200

    Command Line option for browser and url

--- conftest.py
+++ conftest.py
@@ -6,10 +6,12 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url = base_url)
     elif not fixture.is_valid():
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
@@ -19,5 +21,9 @@ def stop(request):
     def fin():
         fixture.session.ensure_logout()
         fixture.destroy()
-
     request.addfinalizer(fin)
+    return fixture
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook")
--- fixture/application.py
+++ fixture/application.py
@@ -4,12 +4,21 @@ from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 
 class Application:
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox(executable_path='/usr/bin/geckodriver')
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+
         self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -20,10 +29,10 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook")
-        if not (wd.current_url.endswith("/addressbook/") or
-                wd.current_url.endswith("/index.php")):
-             wd.get("http://localhost/addressbook")
+        wd.get(self.base_url)
+        #if not (wd.current_url.endswith("/addressbook/") or
+        #        wd.current_url.endswith("/index.php")):
+        #     wd.get("http://localhost/addressbook")
 
     def destroy(self):
         self.wd.quit()
