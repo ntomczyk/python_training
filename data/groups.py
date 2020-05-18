38d88abfdd2d8950485859d6a1920726a25871d4
Nina Tomczyk <tomczyk.nina@gmail.com>
2020-05-18 19:53:52 +0200

    Fixture - generate datatest

---
+++ data/groups.py
@@ -0,0 +1,7 @@
+from model.group import Group
+
+testdata = [
+    Group(group_name= "name1", header="header1", footer="footer1"),
+    Group(group_name="name2", header="header2", footer="footer2")
+]
+
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from model.group import Group
-from fixture.group import *
-import pytest
-#from data.add_group import testdata
-from data.add_group import constant as testdata
 
 
-@pytest.mark.parametrize("group",testdata, ids=[repr(x) for x in testdata])
-def test_add_group_ddd(app,group):
+def test_add_group_ddd(app, data_groups):
+    group = data_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     new_groups = app.group.get_group_list()
